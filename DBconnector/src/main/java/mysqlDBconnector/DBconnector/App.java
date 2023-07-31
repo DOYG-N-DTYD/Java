c350939534a143c591cc53812cd6ff9da8e82208
DOYG-N-DTYD <miroslawzborowski1105@gmail.com>
2023-07-31 10:47:11 +0200

    maven project base data for mysqlDBconnector in java

---
+++ DBconnector/src/main/java/mysqlDBconnector/DBconnector/App.java
@@ -0,0 +1,13 @@
+package mysqlDBconnector.DBconnector;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
