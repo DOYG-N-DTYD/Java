7f2606fc5a7f92d825bf154fb3c583e335f02582
DOYG-N-DTYD <miroslawzborowski1105@gmail.com>
2023-07-31 18:39:19 +0200

    local state before codespaces first run. ANd first pull

--- DBconnector/src/main/java/mysqlDBconnector/DBconnector/App.java
+++ DBconnector/src/main/java/mysqlDBconnector/DBconnector/App.java
@@ -1,13 +1,37 @@
 package mysqlDBconnector.DBconnector;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class App
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+
+    String sqlSelectAllPersons = "SELECT * FROM store";
+    String connectionUrl = "jdbc:mysql://mn29.webd.pl:3306/mzdev_filmstore"; //?serverTimezone=UTC
+    String username = "mzdev_moderator";
+    String password = "stone11051996#A";
+    System.out.println("Init data for credentials");
+    try (Connection conn = DriverManager.getConnection(connectionUrl, username, password);
+           PreparedStatement ps = conn.prepareStatement(sqlSelectAllPersons);
+           ResultSet rs = ps.executeQuery()) {
+
+           while (rs.next()) {
+               //long id = rs.getLong("ID");
+//                String name = rs.getString("name");
+//                int year = rs.getInt("year");
+//                int price = rs.getInt("price");
+
+               //System.out.println("DATA " + "  NAME "+ name + " YEAR " + year +  " PRICE" + price);
+            System.out.print(rs + "\n");
+           }
+    } catch (SQLException e) {
+    System.out.println(e);
+    }
     }
 }
