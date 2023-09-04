9853010b37bc218c58de09368d9dbf3bfa332e2f
DOYG-N-DTYD <miroslawzborowski1105@gmail.com>
2023-09-04 07:00:56 +0200

    ClassArrayList

--- book/src/main/java/SDT/CF/Classes/ClassArrayList.java
+++ book/src/main/java/SDT/CF/Classes/ClassArrayList.java
@@ -1,5 +1,33 @@
 package SDT.CF.Classes;
 
-public class ClassArrayList {
+import java.util.ArrayList;
 
+import OCP.book.chapter_8.Lambda.Animal;
+
+public class ClassArrayList<P> {
+private ArrayList<P> testArrayList = new ArrayList<P>();
+	
+	public ClassArrayList(){	}
+
+	public ArrayList<P> getTestArrayList() {
+		return testArrayList;
+	}
+
+	public void setTestArrayList(ArrayList<P> testArrayList) {
+		this.testArrayList = testArrayList;
+	}
+	
+	@Override
+		public String toString() {
+			// TODO Auto-generated method stub
+			return super.toString();
+		}
 }
+//ClassArrayList<Animal> arrayListAnimals = new ClassArrayList<>();
+//ArrayList<Animal> tempArrayList = arrayListAnimals.getTestArrayList();
+////arrayListAnimals.getTestArrayList().add(new Animal("fish",false,true));
+////arrayListAnimals.getTestArrayList().add(new Animal("rabbit",true,false));
+//tempArrayList.add(new Animal("fish",false,true));
+//tempArrayList.add(new Animal("rabbit",true,false));
+//arrayListAnimals.setTestArrayList(tempArrayList);
+//System.out.println(arrayListAnimals.getTestArrayList().toString());
