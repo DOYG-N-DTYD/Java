1918f32d4414bd4c4b23c83a9c1b6865d3474368
DOYG-N-DTYD <miroslawzborowski1105@gmail.com>
2023-09-04 07:40:34 +0200

    ClassLinkedList

--- book/src/main/java/SDT/CF/Classes/ClassLinkedList.java
+++ book/src/main/java/SDT/CF/Classes/ClassLinkedList.java
@@ -1,5 +1,36 @@
 package SDT.CF.Classes;
 
-public class ClassLinkedList {
+import java.util.LinkedList;
+
+import OCP.book.chapter_8.Lambda.Animal;
+
+public class ClassLinkedList<P> {
+	private LinkedList<P> testLinkedList= new LinkedList<P>();	
+	
+	public ClassLinkedList(){}
+	
+	public LinkedList getLinkedList() {
+		return testLinkedList;
+	};
+	public void setLinkedList(LinkedList<P> testLinkedList) {
+		this.testLinkedList = testLinkedList;
+	};
 
 }
+//
+//ClassLinkedList<Animal> classLinkedListAnimals = new ClassLinkedList<Animal>();
+//LinkedList<Animal> tempLinkedList = classLinkedListAnimals.getLinkedList();
+//tempLinkedList.add(new Animal("fish",false,true));
+//tempLinkedList.add(new Animal("rabbit",true,false));
+//classLinkedListAnimals.setLinkedList(tempLinkedList);
+//System.out.println(classLinkedListAnimals.getLinkedList().toString());
+
+
+//LinkedList<String> testLinkedList = new LinkedList<String>();
+//testLinkedList.addFirst("A");
+//testLinkedList.addLast("F");
+//testLinkedList.addFirst("H");
+//testLinkedList.addLast("H");
+//System.out.println(testLinkedList.toString());
+//System.out.println(testLinkedList.removeLastOccurrence("H"));
+//System.out.println(testLinkedList.toString());
