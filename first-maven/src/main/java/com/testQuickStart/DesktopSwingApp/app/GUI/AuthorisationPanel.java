9857fb09930f06112ea6039bd96941cb3f2e4ee4
Tereza Zbarouskaya <tpapko@sgh.waw.pl>
2023-12-12 08:21:14 +0100

    Authorisation panel divided in small methods

--- first-maven/src/main/java/com/testQuickStart/DesktopSwingApp/app/GUI/AuthorisationPanel.java
+++ first-maven/src/main/java/com/testQuickStart/DesktopSwingApp/app/GUI/AuthorisationPanel.java
@@ -10,29 +10,62 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class AuthorisationPanel {
-	public JPanel authorisationPanel;
-	
+	private JPanel authorisationPanel;
+	private JTextField usernameField;
+	private JTextField passwordField;
+	private JButton connectButton;
+	private JButton fetchDataButton;
+
 	public AuthorisationPanel(){
 	//  Create a panel for database connection controls on the left side
-	    JPanel dbPanel = new JPanel();
+		authorisationPanel =  new JPanel();
 	    //dbPanel.setLayout(new BoxLayout(dbPanel, BoxLayout.Y_AXIS));
 	    //dbPanel.setPreferredSize(new Dimension(25, 10));
 
-	    JTextField usernameField = new JTextField(10);
-	    JTextField passwordField = new JTextField(10);
+	    createButtons();
+		addEventsToButtons();
+		initAuthorisationPanel();
+	}
+	private void initAuthorisationPanel(){
+		authorisationPanel.add(new JLabel("Database Connection"));
+	    authorisationPanel.add(new JLabel("Username:"));
+	    authorisationPanel.add(usernameField);
+	    authorisationPanel.add(new JLabel("Password:"));
+	    authorisationPanel.add(passwordField);
+	    authorisationPanel.add(connectButton);
+	    authorisationPanel.add(fetchDataButton);
+	}
 
-	    JButton connectButton = new JButton("Connect");
-	    JButton fetchDataButton = new JButton("Fetch Data");
+	private void createButtons(){
+		Integer inputFieldsSize = 10;
+		String connectButtonName = "Connect";
+		String fetchButtonName = "Fetch Data";
 
-	    dbPanel.add(new JLabel("Database Connection"));
-	    dbPanel.add(new JLabel("Username:"));
-	    dbPanel.add(usernameField);
-	    dbPanel.add(new JLabel("Password:"));
-	    dbPanel.add(passwordField);
-	    dbPanel.add(connectButton);
-	    dbPanel.add(fetchDataButton);
-	    
-	    authorisationPanel = dbPanel;
-	    
+		usernameField = new JTextField(inputFieldsSize);
+		passwordField = new JTextField(inputFieldsSize);
+		
+	    connectButton = new JButton(connectButtonName);
+	    fetchDataButton = new JButton(fetchButtonName);
+	}
+	private void addEventsToButtons(){
+		       connectButton.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               String username = usernameField.getText();
+               String password = passwordField.getText();
+               // Implement your database connection logic here
+               // You can use username and password to establish a connection
+               // to your database (e.g., MySQL, PostgreSQL, etc.).
+           }
+       });
+
+       fetchDataButton.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               // Implement your data retrieval logic here
+               // You can use the established database connection to fetch data
+               // and display it in the table.
+           }
+       });
 	}
 }
--- first-maven/src/main/java/com/testQuickStart/DesktopSwingApp/app/SwingDesktopApplication.java
+++ first-maven/src/main/java/com/testQuickStart/DesktopSwingApp/app/SwingDesktopApplication.java
@@ -37,26 +37,6 @@ public class SwingDesktopApplication {
 //        JScrollPane tableScrollPane = new JScrollPane(table);
 //        tableScrollPane.setPreferredSize(new Dimension(100, 100));
 //        applicationFrame.add(tableScrollPane, BorderLayout.SOUTH);
-
-//        connectButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                String username = usernameField.getText();
-//                String password = passwordField.getText();
-//                // Implement your database connection logic here
-//                // You can use username and password to establish a connection
-//                // to your database (e.g., MySQL, PostgreSQL, etc.).
-//            }
-//        });
-//
-//        fetchDataButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                // Implement your data retrieval logic here
-//                // You can use the established database connection to fetch data
-//                // and display it in the table.
-//            }
-//        });
     }
 
 }
