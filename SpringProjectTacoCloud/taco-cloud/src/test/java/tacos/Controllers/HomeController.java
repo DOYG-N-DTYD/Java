470d61d2630d479810ae33ea8c137ea319cac66b
DOYG-N-DTYD <miroslawzborowski1105@gmail.com>
2024-02-09 15:46:51 +0100

    homepage.html, homepage image

--- SpringProjectTacoCloud/taco-cloud/src/test/java/tacos/Controllers/HomeController.java
+++ SpringProjectTacoCloud/taco-cloud/src/test/java/tacos/Controllers/HomeController.java
@@ -1,5 +1,18 @@
 package tacos.Controllers;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import net.bytebuddy.asm.Advice.Return;
+
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		return	 "home";
+	}
+	
 }
