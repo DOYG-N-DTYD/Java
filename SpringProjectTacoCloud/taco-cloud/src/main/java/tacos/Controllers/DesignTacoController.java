80cf4c7ee9de92983efc89cc36c71b121a217354
DOYG-N-DTYD <miroslawzborowski1105@gmail.com>
2024-02-11 16:14:37 +0100

    Stream wrapper

--- SpringProjectTacoCloud/taco-cloud/src/main/java/tacos/Controllers/DesignTacoController.java
+++ SpringProjectTacoCloud/taco-cloud/src/main/java/tacos/Controllers/DesignTacoController.java
@@ -3,7 +3,9 @@ package tacos.Controllers;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -19,6 +21,7 @@ import tacos.Classes.Ingredient;
 import tacos.Classes.Ingredient.Type;
 import tacos.Classes.Taco;
 import tacos.Classes.TacoOrder;
+import tacos.Interfaces.IngredientRepository;
 
 @Slf4j
 //аннотация добавляет в класс
@@ -37,14 +40,22 @@ public class DesignTacoController {
 	private static final org.slf4j.Logger log =
 			 org.slf4j.LoggerFactory.getLogger(DesignTacoController.class);
 	
+	private final IngredientRepository ingredientRepo;
+	
+	@Autowired
+	public DesignTacoController(IngredientRepository ingredientRepo) {
+		this.ingredientRepo = ingredientRepo;
+	}
+	
 	@ModelAttribute
 	public void addIngredientsToModel(Model model) {
-		List<Ingredient> ingredients = Arrays.asList(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-				new Ingredient("COTO", "Corn Tortilla", Type.WRAP), new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-				new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-				new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES), new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-				new Ingredient("CHED", "Cheddar", Type.CHEESE), new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-				new Ingredient("SLSA", "Salsa", Type.SAUCE), new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+//		List<Ingredient> ingredients = Arrays.asList(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+//				new Ingredient("COTO", "Corn Tortilla", Type.WRAP), new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+//				new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+//				new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES), new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+//				new Ingredient("CHED", "Cheddar", Type.CHEESE), new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+//				new Ingredient("SLSA", "Salsa", Type.SAUCE), new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+		Iterable<Ingredient> ingredients = ingredientRepo.findAll();
 		Type[] types = Ingredient.Type.values();
 		for (Type type : types) {
 			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
@@ -66,17 +77,16 @@ public class DesignTacoController {
 		return "design";
 	}
 
-	private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
-		return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
+	private Iterable<Ingredient> filterByType(Iterable<Ingredient> ingredients, Type type) {
+		// Iterable has a spliterator() method, which can pass to StreamSupport.stream to create a stream:
+		return StreamSupport.stream(ingredients.spliterator(), false).filter(x -> x.getType().equals(type)).collect(Collectors.toList());
+		//return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
 	}
 
 	@PostMapping
 	// @PostMapping -> сообщает @RequestMapping на уровне класса, что processTaco() обрабатывает запросы POST с путём /design
 	public String processTaco(@Valid Taco taco,Errors errors, @ModelAttribute TacoOrder tacoOrder) {	// использовать объект TacoOrder, который был помещен в модель методом order() 
 		if (errors.hasErrors()) {
-			log.info("ERROR ! " + errors); 
-			log.info("ERROR ! " + taco.getName());
-			log.info("ERROR ! " + taco.getIngredients());
 			return "design";
 			 }
 		tacoOrder.addTaco(taco);
