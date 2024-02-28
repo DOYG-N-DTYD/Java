bc8028eaf148e25fe57cefebfa36679471850817
DOYG-N-DTYD <miroslawzborowski1105@gmail.com>
2024-02-28 18:39:01 +0100

    mod Ingredient.class
    Artgs No ArgsConstructor -> attributes final

--- SpringProjectTacoCloud/taco-cloud/src/main/java/tacos/Classes/Ingredient.java
+++ SpringProjectTacoCloud/taco-cloud/src/main/java/tacos/Classes/Ingredient.java
@@ -3,40 +3,33 @@ package tacos.Classes;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 
-@Data
+//@Data
 //@RequiredArgsConstructor
 //@NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
 //@Entity
 
-@Entity
-@Builder 
-@NoArgsConstructor 
-@AllArgsConstructor
-@Setter(value = AccessLevel.PACKAGE)
-@Getter
+//@Entity
+//@Builder 
+//@NoArgsConstructor 
+//@AllArgsConstructor
+//@Setter(value = AccessLevel.PACKAGE)
+//@Getter
 
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
+@Entity
 public class Ingredient {//implements Persistable<String>{
 	@Id
-	private String id;
-	private String name;
-	private Type type;
-	
-//	public Ingredient(String idString, String nameString, Type ingridientType) {
-//		this.id = idString;
-//		this.name = nameString;
-//		this.type = ingridientType;
-//	}
+	private final String id;
+	private final String name;
+	private final Type type;
 	
 	public enum Type {
 		WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
 	}
-	
-	
 }
